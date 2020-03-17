a7a60332050681da026e4c77e9372e1a3bbd4e10
Valerie Schnapp <valerie.f.schnapp@gmail.com>
2020-03-17 12:15:18 -0400

    tests updated

--- datum/tests/test_adb.py
+++ datum/tests/test_adb.py
@@ -1,15 +1,33 @@
 import logging
 import sys
 import os
+from pathlib import Path
 
 import time
 import pytest
 import numpy as np
 
+from datum.create import create_db
 from datum.access import Access
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+
+
+def data_loc(file_name):
+    pardir = Path(__file__).parents[0]
+    return os.path.join(pardir, file_name)
+
+
+def test_generate_db(table_name):
+    start = time.time()
+    if not os.path.exists(data_loc("data.db")):
+        logger.debug("This will take a minute...")
+        create_db("athlete_events.csv", "data.db", table_name, data_loc)
+        logger.debug("Database generated!")
+    logger.debug("time: {:6.4f}".format(time.time() - start))
+
+    assert os.path.exists(data_loc("data.db"))
 
 
 def test_adb(file_path, table_name, my_query):
@@ -19,4 +37,5 @@ def test_adb(file_path, table_name, my_query):
     logger.debug("time: {:6.4f}".format(time.time() - start))
     logger.debug("Sex: {}".format(results.iloc[0].Sex))
     logger.debug(results.Sex)
+
     assert np.all(results.Sex == "F")
